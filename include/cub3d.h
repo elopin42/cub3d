3a92ee93fda19b964e5725d64bca3298d61b86d3
OSS-283 <leo151297@gmail.com>
2025-08-27 16:24:05 +0200

    Update cub3d.h

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: lle-cout <lle-cout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 16:06:00 by elopin            #+#    #+#             */
-/*   Updated: 2025/08/27 02:12:05 by lle-cout         ###   ########.fr       */
+/*   Updated: 2025/08/27 16:23:15 by lle-cout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -118,55 +118,70 @@ int				mouse_moved_advanced(int x, int y, void *param);
 int				key_press(int keycode, t_global *glb);
 int				key_release(int keycode, t_global *glb);
 int				update(t_global *glb);
+void			load_texture(void *mlx, t_img *tex, char *path);
 
 // calcul_for_draw.c
-void			calculate_wall_distance(t_global *glb);
-void			perform_dda(t_global *glb);
-void			calculate_step_and_side_dist(t_global *glb);
 void			init_ray(t_global *glb, int x);
+void			calculate_step_and_side_dist(t_global *glb);
+void			perform_dda(t_global *glb);
+void			calculate_wall_distance(t_global *glb);
 void			perform_dda_ignoring_doors(t_global *glb, t_ray *ray);
 
-// draw_wall_tex.c
-void			draw_wall_texture(t_global *glb, int x, t_img *tex);
-unsigned int	get_normal_wall_color(t_global *glb, t_img *tex, int tex_x,
-					int tex_y);
-unsigned int	apply_distance_effect(unsigned int color, double dist);
-unsigned int	handle_door_opening(t_global *glb, int x, int y);
-void			calculate_texture_params(t_global *glb, t_img *tex,
-					int *tex_x, double *step);
+// cam_moove.c
+void	rotate_camera(t_global *glb, double angle);
+bool	set_xy_for_move(t_global *glb, double *new_x, double *new_y,
+		int direction);
+void	move_player(t_global *glb, int direction);
 
-// torch and effects
-void			draw_torch(t_global *glb);
+// door.c
+bool			set_xy(t_global *glb, int y, int x);
+bool			check_door_acces(t_global *glb, int y, int x, char c);
+void			ft_door(t_global *glb);
+
+// draw.c
+void			draw_ceiling_and_sky(t_global *glb, int x);
+void			draw_floor(t_global *glb, int x);
+void			draw_vertical_line(t_global *glb, int x);
+void			draw_scene(t_global *glb);
+
+// draw_wall_tex2.c
+unsigned int	apply_distance_effect(unsigned int color, double dist);
+unsigned int	get_normal_wall_color(t_global *glb, t_img *tex,
+		int tex_x, int tex_y);
+unsigned int	handle_door_opening(t_global *glb, int x, int y);
+void	calculate_texture_params(t_global *glb, t_img *tex, int *tex_x,
+		double *step);
+
+// draw_wall_tex.c
 void			calculate_secondary_ray(t_global *glb);
 unsigned int	draw_wall_behind_door(t_global *glb, int y);
 unsigned int	handle_sky_part(t_global *glb, int x, int y);
 unsigned int	handle_floor_part(t_global *glb, int y);
-unsigned int	effet_noir(unsigned int color, double factor);
+void			draw_wall_texture(t_global *glb, int x, t_img *tex);
 
-// texture selection
-t_img			*select_wall_texture_from_ray(t_global *glb, t_ray *ray);
+// frame.c
+void			frame_for_flame(t_global *glb);
+void			draw_torch(t_global *glb);
+int				lunch_frame(t_global *glb);
+
+// ft_clean_up.c
+void			ft_free_map(char **map);
+void			ft_clean_imagouille(void *mlx, t_img *tex);
+void			ft_clean_all(t_global *glb, int exit_code);
+
+// minimap.c
+void			draw_square(t_global *glb, int x, int y, int d);
+void			draw_minimap(t_global *glb);
+void			set_map_dimensions(t_global *glb);
+
+// put_texture.c
+void			put_pixel(t_img *img, int x, int y, int color);
 t_img			*select_wall_texture(t_global *glb);
+t_img			*select_wall_texture_from_ray(t_global *glb, t_ray *ray);
 
-// utils and validation
+// small_function.c
+unsigned int	effet_noir(unsigned int color, double factor);
 bool			is_valid_map_position(t_global *glb, int x, int y);
 long			get_current_time_ms(void);
-bool			check_door_acces(t_global *glb, int y, int x, char c);
-
-// core functions
-void			ft_free_map(char **map);
-void			ft_door(t_global *glb);
-void			ft_clean_all(t_global *glb, int exit_code);
-void			set_map_dimensions(t_global *glb);
-void			frame_for_flame(t_global *glb);
-void			put_pixel(t_img *img, int x, int y, int color);
-void			load_texture(void *mlx, t_img *tex, char *path);
-int				lunch_frame(t_global *glb);
-bool			ft_init(t_global *glb, char **av);
-void			draw_minimap(t_global *glb);
-bool			init_texture(t_global *glb);
-bool			ft_parsing(t_global *glb);
-void			draw_scene(t_global *glb);
-void			rotate_camera(t_global *glb, double angle);
-void			move_player(t_global *glb, int direction);
 
 #endif
