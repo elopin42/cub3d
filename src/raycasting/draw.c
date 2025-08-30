352f1a071438ab904125f622f906b46c2299214a
elopin42 <ethanlo0506@gmail.com>
2025-08-30 16:45:00 +0200

    optimisation des couleur sol et sky pour pas faire de calcul dans le vide

--- src/raycasting/draw.c
+++ src/raycasting/draw.c
@@ -22,12 +22,15 @@ void	draw_ceiling_and_sky(t_global *glb, int x)
 	int (y) = -1;
 	while (++y < glb->h / 2)
 	{
-		tex_x = (x * glb->texture.sky.width) / glb->w;
-		tex_y = (y * glb->texture.sky.height) / (glb->h / 2);
-		pixel = glb->texture.sky.addr + (tex_y * glb->texture.sky.line_length
-				+ tex_x * (glb->texture.sky.bpp / 8));
-		color = *(unsigned int *)pixel;
-    if (glb->img.is_rgb)
+    if (!glb->img.is_rgb)
+    {
+		  tex_x = (x * glb->texture.sky.width) / glb->w;
+		  tex_y = (y * glb->texture.sky.height) / (glb->h / 2);
+		  pixel = glb->texture.sky.addr + (tex_y * glb->texture.sky.line_length
+			  	+ tex_x * (glb->texture.sky.bpp / 8));
+		  color = *(unsigned int *)pixel;
+    }
+    else
       color = ft_uni(*glb->img.rgb);
 		put_pixel(&glb->img, x, y, color);
 	}
@@ -47,18 +50,21 @@ void	draw_floor(t_global *glb, int x)
 	while (++y < glb->h)
 	{
 		dist = glb->h / (2.0 * y - glb->h);
-		floor_x = glb->player.x + dist * glb->ray.ray_dir_x;
-		floor_y = glb->player.y + dist * glb->ray.ray_dir_y;
-		tex_x = (int)(floor_x * glb->texture.sol.width)
-			% glb->texture.sol.width;
-		tex_y = (int)(floor_y * glb->texture.sol.height)
-			% glb->texture.sol.height;
-		pix = glb->texture.sol.addr + tex_y * glb->texture.sol.line_length
-			+ tex_x * (glb->texture.sol.bpp / 8);
-		color = *(unsigned int *)pix;
-		color = apply_distance_effect(color, dist);
-    if (glb->img.is_rgb)
+    if (!glb->img.is_rgb)
+    {
+		  floor_x = glb->player.x + dist * glb->ray.ray_dir_x;
+		  floor_y = glb->player.y + dist * glb->ray.ray_dir_y;
+		  tex_x = (int)(floor_x * glb->texture.sol.width)
+			  % glb->texture.sol.width;
+		  tex_y = (int)(floor_y * glb->texture.sol.height)
+			  % glb->texture.sol.height;
+		  pix = glb->texture.sol.addr + tex_y * glb->texture.sol.line_length
+			  + tex_x * (glb->texture.sol.bpp / 8);
+		  color = *(unsigned int *)pix;
+    }
+    else
       color = ft_uni(*glb->img.rgb);
+		color = apply_distance_effect(color, dist);
 		put_pixel(&glb->img, x, y, color);
 	}
 }
