0bfdb1af4e5f45cd11086bfa1f8fe0fb5a9614c4
OSS-283 <leo151297@gmail.com>
2025-09-10 12:04:00 +0200

    Fix: reverted north/south textures orientation

--- src/raycasting/put_texture.c
+++ src/raycasting/put_texture.c
@@ -6,7 +6,7 @@
 /*   By: lle-cout <lle-cout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 16:41:37 by elopin            #+#    #+#             */
-/*   Updated: 2025/08/27 14:21:40 by lle-cout         ###   ########.fr       */
+/*   Updated: 2025/09/10 11:57:11 by lle-cout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ t_img	*select_wall_texture(t_global *glb)
 	if (glb->ray.side == 0 && glb->ray.ray_dir_x < 0)
 		return (&glb->texture.est);
 	if (glb->ray.side == 1 && glb->ray.ray_dir_y > 0)
-		return (&glb->texture.sud);
-	return (&glb->texture.nord);
+		return (&glb->texture.nord);
+	return (&glb->texture.sud);
 }
 
 t_img	*select_wall_texture_from_ray(t_global *glb, t_ray *ray)
